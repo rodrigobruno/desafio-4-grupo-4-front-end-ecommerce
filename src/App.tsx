d9e4980f5f4047d984e187178a45bc75d4a45d2d
rodrigobruno <rodrigo@rodrigobruno.com>
2023-05-31 15:31:29 -0300

    Configurado a fonte Barlow como fonte padrão do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { Button } from 'react-bootstrap';
-
 export default function App() {
     return (
         <>
-            <Button variant='primary'>Primary</Button>
-            <Button variant='primary' size='lg' disabled>
-                Primary
-            </Button>
-            {/* <Button variant='secondary'>Secondary</Button>
-            <Button variant='success'>Success</Button>
-            <Button variant='warning'>Warning</Button>
-            <Button variant='danger'>Danger</Button>
-            <Button variant='info'>Info</Button>
-            <Button variant='light'>Light</Button>
-            <Button variant='dark'>Dark</Button>
-            <Button variant='link'>Link</Button> */}
+            <h1>Teste</h1>
         </>
     );
 }
