24e6d395ecf12431f9e0b50793c3d29a4197e554
rodrigobruno <rodrigo@rodrigobruno.com>
2023-05-31 15:11:25 -0300

    Configurado o Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { Button } from 'react-bootstrap';
+
 export default function App() {
-    return <h1>Gama Zone</h1>;
+    return (
+        <>
+            <Button variant='primary'>Primary</Button>
+            <Button variant='primary' size='lg' disabled>
+                Primary
+            </Button>
+            {/* <Button variant='secondary'>Secondary</Button>
+            <Button variant='success'>Success</Button>
+            <Button variant='warning'>Warning</Button>
+            <Button variant='danger'>Danger</Button>
+            <Button variant='info'>Info</Button>
+            <Button variant='light'>Light</Button>
+            <Button variant='dark'>Dark</Button>
+            <Button variant='link'>Link</Button> */}
+        </>
+    );
 }
